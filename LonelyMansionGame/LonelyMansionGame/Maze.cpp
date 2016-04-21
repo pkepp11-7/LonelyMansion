1ab16c1e651a1d0bf06a309bb27d93ab174047db
pkepp11-7 <patrick.keppler@wsu.edu>
2016-04-21 15:19:36 -0700

    Adds in a floor plan resource file, and  implements a functional constructor for the maze

--- LonelyMansionGame/LonelyMansionGame/Maze.cpp
+++ LonelyMansionGame/LonelyMansionGame/Maze.cpp
@@ -2,22 +2,21 @@
 
 Maze::Maze()
 {
-	int i = 0, j = 0, k = 0;
+	fstream infile("Maze Floor Plans.txt");
+	int i, j, k;
 	for (i = 0; i < FLOORS; i++)
 	{
 		for (j = 0; j < MAZE_SIZE; j++)
 		{
 			for (k = 0; k < MAZE_SIZE; k++)
 			{
-				mMansion[i][j][k] = 0;
+				infile >> mMansion[i][j][k];
 			}
 		}
 	}
-	i = rand() % FLOORS;
-	j = rand() % MAZE_SIZE;
-	k = rand() % MAZE_SIZE;
-
-	//set
+	infile.close();
+	placeVictoryRoom();
+	placeStairs();
 
 }
 
@@ -25,4 +24,49 @@ bool weightedChance(int percentage)
 {
 	int num = rand() % 100;
 	return num < percentage;
+}
+
+void Maze::placeVictoryRoom(void)
+{
+	int i = 0, j = 0, k = 0;
+	bool success = false;
+	while (!success)
+	{
+		i = rand() % FLOORS;
+		j = rand() % MAZE_SIZE;
+		k = rand() % MAZE_SIZE;
+		if (mMansion[i][j][k] == 9)
+		{
+			success = true;
+			mMansion[i][j][k] = 4;
+		}
+	}
+	
+}
+
+void Maze::placeStairs(void)
+{
+	int j = 0, k = 0;
+	bool success = false;
+	while (!success)
+	{
+		j = rand() % MAZE_SIZE;
+		k = rand() % MAZE_SIZE;
+		if (mMansion[0][j][k] == mMansion[1][j][k] == 9)
+		{
+			success = true;
+			mMansion[0][j][k] = mMansion[1][j][k] = 2;
+		}
+	}
+	success = false;
+	while (!success)
+	{
+		j = rand() % MAZE_SIZE;
+		k = rand() % MAZE_SIZE;
+		if (mMansion[1][j][k] == mMansion[2][j][k])
+		{
+			success = true;
+			mMansion[1][j][k] = mMansion[2][j][k] = 3;
+		}
+	}
 }
--- LonelyMansionGame/LonelyMansionGame/Maze.h
+++ LonelyMansionGame/LonelyMansionGame/Maze.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <time.h>
 #include <cstdlib>
+#include <fstream>
+
+using std::fstream;
 
 //adjust maze size here if needed
 #define MAZE_SIZE 10
@@ -12,8 +15,10 @@ class Maze
 {
 private:
 	int mMansion[FLOORS][MAZE_SIZE][MAZE_SIZE];
+	void placeVictoryRoom(void);
+	void placeStairs(void);
 public:
 	Maze();
 };
 
-bool weightedAverage(int percentage);
+bool weightedChance(int percentage);
