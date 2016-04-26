75353c9cda8d2e107ba60ef0476412cfc1c7c1b3
SassyNinja <jkholm@live.com>
2016-04-25 18:33:45 -0700

    Another update

--- LonelyMansionGame/LonelyMansionGame/player.h
+++ LonelyMansionGame/LonelyMansionGame/player.h
@@ -14,15 +14,18 @@ public:
 	Player(int newHitPoints = 3, int newAttackAccuracy = 100, int newWeaponDamage = 0, string newWeaponName = "short sword");
 	~Player() {};
 	void setHitPoints(int newHitPoints);
+	void setHitEvasiveness(int newHitEvasiveness);
 	void setAttackAccuracy(int newAttackAccuracy);
 	void setWeaponDamage(int newWeaponDamage);
 	void setWeaponName(string newWeaponName);
 	int getHitPoints();
+	int getHitEvasiveness();
 	int getAttackAccuracy();
 	int getWeaponDamage();
 	string getWeaponName();
 private:
 	int hitPoints;
+	int hitEvasiveness;
 	int attackAccuracy;
 	int weaponDamage;
 	string weaponName;
